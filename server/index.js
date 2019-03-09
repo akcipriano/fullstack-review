3cd326f7da45ccf8be817b38fead6bd40c9cec76
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-03-08 21:56:08 -0800

    Complete get to repos endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const getData = require('../helpers/github.js');
 const insertToMongo = require('../database/index.js');
+var MongoClient = require('mongodb').MongoClient;
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -33,15 +34,57 @@ app.post('/repos', function (req, res) {
 
   res.status(200).send('Server post success');
 });
+var bigToSmall = (array) => {
+  for (var i = 0; i < array.length - 1; i++) {
+    // console.log('bigtosmall', array[i].stars)
+    var big = array[i];
+    var small = array[i + 1];
+    if(big.stars < small.stars) {
+      array[i] = small
+      array[i+1] = big;
+      return bigToSmall(array);
+    }
+  }
+  return array;
+}
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  MongoClient.connect('mongodb://localhost:27017/fetcher', {useNewUrlParser: true}, function(err, client) {
+    if (err) throw err;
+
+    var db = client.db('fetcher');
+    db.collection('repos').find().toArray(function(err, result) {
+      if (err) throw err;
+
+      //this creates an object with a key-value pair for star count and url which is stored in an array
+      var objResults = [];
+      result.forEach(rep => {
+        var obj = {};
+        obj['stars'] = rep.stargazer_count;
+        obj['url'] = rep.html_url;
+        objResults.push(obj);
+      });
+      //this orders the array with objects from most stars to least stars
+      var ex = bigToSmall(objResults);
+      //stores the top 25 repos (highest stars)
+      var topTwentyFive = [];
+      for (var i = 0; i < 25; i++) {
+        if (ex[i]) {
+          topTwentyFive.push(ex[i]);
+        }
+      }
+      console.log('big2small', ex);
+      console.log('DB RESULTS', objResults);
+      res.status(200).send(topTwentyFive);
+    });
+  });
 });
+
 
 let port = 1128;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
